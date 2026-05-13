46071a48de995ffbebfccaaa5a756b1f50a12bc6
Swoyam <mhrzswoyam15@gmail.com>
2026-05-13 12:01:14 +0545

    Donate Frontend

---
+++ src/main/java/com/pawsypaila/controller/LoginServlet.java
@@ -0,0 +1,92 @@
+package com.pawsypaila.controller;
+
+import jakarta.servlet.ServletException;
+
+
+
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.pawsypaila.dao.UserDAO;
+import com.pawsypaila.model.UserModel;
+import com.pawsypaila.utils.PasswordUtil;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet (asyncSupported = true, urlPatterns = { "/login" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+    	request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+    	
+    	String email    = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        try {
+            UserDAO userDAO = new UserDAO();
+
+            // 2. Fetch user from DB by email
+            UserModel user = userDAO.getUserByEmail(email);
+
+            if (user == null) {
+                // No user found with that email
+                System.out.println("User not found!");
+                response.sendRedirect(request.getContextPath() + "/login?error=1");
+                return;
+            }
+
+            // 3. Check password using PasswordUtil
+            boolean passwordMatch = PasswordUtil.checkPassword(password, user.getPassword());
+
+            if (passwordMatch) {
+                // 4. Password correct — create session
+                System.out.println("Login successful!");
+                HttpSession session = request.getSession();
+                session.setAttribute("user", user);// save whole user object
+                response.sendRedirect(request.getContextPath() + "/home");
+               
+
+            } else {
+                // Wrong password
+                System.out.println("Wrong password!!!");
+                response.sendRedirect(request.getContextPath() + "/login?error=2");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+            response.sendRedirect(request.getContextPath() + "/login?error=3");
+        }
+    }
+		
+}
---
+++ src/main/webapp/donate.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="${pageContext.request.contextPath}/css/donation.css">
+<title>Donation</title>
+</head>
+<body>
+<%@ include file="header.jsp"%>
+	<div class="main-container">
+	<div class="card">
+		<div class="top-paw"></div>
+		<div class="icon"></div>
+		
+		<h1>Donate</h1>
+		<p>Your donation saves lives. 100% goes directly to shelter care, vet bills, and adoptive programs.</p>
+		
+		<form action="${pageContext.request.contextPath}/donation" modelAttribute="donation" method="Post">
+			<input path="name" placeholder="Name" class="form-control">
+			<input path="amount" placeholder="Amount" class="form-control">
+			<input path="paymentMethod" placeholder="Payment Method" class="form-control">
+			<button type="submit" class="Donate-button">Donate</button>
+		</form>
+		
+		<div class="bottom-paw"></div>
+		</div>
+	</div>
+</body>
+</html>

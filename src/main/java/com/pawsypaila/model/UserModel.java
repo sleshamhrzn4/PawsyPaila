4549062bf01694d9686b254589280da24e37bf44
noko <sulavstha00@gmail.com>
2026-05-18 13:22:38 +0545

    updated register.css

--- src/main/java/com/pawsypaila/model/UserModel.java
+++ src/main/java/com/pawsypaila/model/UserModel.java
@@ -22,7 +22,7 @@ public class UserModel {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-    public void setemail(String email) { this.email = email; } // backward-compat alias
+    public void setemail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }

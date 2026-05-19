23c4bc526e7291fa64bffb7f5552e203a6d81893
noko <sulavstha00@gmail.com>
2026-05-19 14:42:24 +0545

    added home page jsp, css and servlet

--- src/main/java/com/pawsypaila/controller/HomeServlet.java
+++ src/main/java/com/pawsypaila/controller/HomeServlet.java
@@ -1,12 +1,17 @@
 package com.pawsypaila.controller;
 
-import jakarta.servlet.ServletException;
+import com.pawsypaila.dao.PetDAO;
+import com.pawsypaila.dao.ProductDAO;
+import com.pawsypaila.model.PetModel;
+import com.pawsypaila.model.ProductModel;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Servlet implementation class HomeServlet
@@ -29,7 +34,21 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		 request.getRequestDispatcher("/WEB-INF/pages/public/home.jsp").forward(request, response);
+
+		try {
+		    PetDAO petDAO = new PetDAO();
+		    ProductDAO productDAO = new ProductDAO();
+
+		    List<PetModel> petList = petDAO.getAllPets();
+		    List<ProductModel> productList = productDAO.getAllProducts();
+
+		    request.setAttribute("petList", petList);
+		    request.setAttribute("itemList", productList);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+
+		request.getRequestDispatcher("/WEB-INF/pages/public/home.jsp").forward(request, response);
 	}
 
 	/**
@@ -39,5 +58,4 @@ public class HomeServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
